7fccb11c3c0e90177cdc85388ef2afe450c77946
M Jibran Zada <mjibranzada@gmail.com>
2025-07-28 22:48:58 +0500

    Added all Html, Css and Js and their perspective folders

---
+++ JS/about.js
@@ -0,0 +1,35 @@
+// ======================== Navbar Animation =================================
+
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll("header nav a");
+
+  navLinks.forEach(link => {
+    link.classList.add("nav-hidden");
+  });
+
+  navLinks.forEach((link, index) => {
+    setTimeout(() => {
+      link.classList.add("nav-reveal");
+    }, 300 + index * 200); // staggered animation
+  });
+});
+
+
+
+
+// Toggle mobile menu
+document.addEventListener("DOMContentLoaded", () => {
+  const menuIcon = document.getElementById("menu-icon");
+  const mobileNav = document.getElementById("mobileNav");
+
+  menuIcon.addEventListener("click", () => {
+    mobileNav.classList.toggle("active");
+  });
+
+  // Optional: close menu if any link clicked
+  mobileNav.querySelectorAll("a").forEach(link => {
+    link.addEventListener("click", () => {
+      mobileNav.classList.remove("active");
+    });
+  });
+});
